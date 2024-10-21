bd67b52863aaa6023a39b07c9c967cd93347bd36
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-21 20:40:37 +0530

    MONGO ADD

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,28 @@ const port = process.env.PORT || 4000;
 
 // laitonjamsanalembameitei99
 //jhSzk7Q881b7RMAO
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://laitonjamsanalembameitei99:jhSzk7Q881b7RMAO@yoga-master.mc6jh.mongodb.net/?retryWrites=true&w=majority&appName=yoga-master";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Rom Start')
