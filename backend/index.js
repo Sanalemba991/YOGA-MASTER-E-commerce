13e8b83fd3b92781152fdd4398614090f1cb5449
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-10-21 20:30:57 +0530

    MONGODB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
+
+// laitonjamsanalembameitei99
+//jhSzk7Q881b7RMAO
 
 app.get('/', (req, res) => {
   res.send('Rom Start')
